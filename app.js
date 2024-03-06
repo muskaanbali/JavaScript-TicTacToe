18d704588ccd5b42df02b13035d4cb6d92bd20f1
muskaanbali <37337504+muskaanbali@users.noreply.github.com>
2024-03-06 05:49:11 +0530

    Giving Player option to choose their symbol.

--- app.js
+++ app.js
@@ -3,9 +3,49 @@ let resetBtn = document.querySelector("#resetBtn")
 let newBtn = document.querySelector("#newBtn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
+let player1 = "";
+let player2 = "";
+let btnX = document.getElementById("xBtn");
+let btnO = document.getElementById("oBtn");
+let turnO = false;
 
-//player X, Player O
-let turnO = true;
+btnX.addEventListener("click", () =>{
+    if(player1 == "" )
+    {
+        player1 = "X";
+        player2 = "O";
+        turnO = false;
+        console.log(`player1 is ${player1} and player2 is ${player2}`);
+        hideSelectionDiv();
+        
+    }
+
+});
+function hideSelectionDiv()
+{
+    var element = document.getElementsByClassName("select-symbol-container");
+        for(let ele of element)
+        {
+            console.log(`ele is ${ele}`);
+            ele.classList.add("hide");
+        }
+
+}
+
+btnO.addEventListener("click", () =>{
+    
+    if(player1 == "" )
+    {
+        player1 = "O";
+        player2 = "X";
+        turnO = true;
+        console.log(`player1 is ${player1} and player2 is ${player2}`);
+        hideSelectionDiv()
+    }
+
+});
+
+
 let count = 0;
 
 const winningPatterns = [
@@ -29,9 +69,11 @@ const resetGame = () =>{
 boxes.forEach( (box) =>{
     box.addEventListener("click", () =>{
         console.log("box clicked");
+        console.log(`turnO is ${turnO}`);
         if(turnO)
         {
             box.innerText = "O";
+            box.style.color = "blue";
             turnO = false;  
         }
         else{
@@ -75,21 +117,17 @@ const showWinner = (winner) =>{
     disableBoxes();
 }
 
-const checkWinner = () => {
-    for(let pattern of winningPatterns)
-    {
+function checkWinner() {
+    for (let pattern of winningPatterns) {
         //we have an array of boxes and array of patterns
         //console.log(boxes[pattern[0]].innerText, boxes[pattern[1]].innerText,
-          //  boxes[pattern[2]].innerText);
-        
+        //  boxes[pattern[2]].innerText);
         let pos1 = boxes[pattern[0]].innerText;
         let pos2 = boxes[pattern[1]].innerText;
         let pos3 = boxes[pattern[2]].innerText;
 
-        if( pos1 != "" && pos2 != "" && pos3 != "" )
-        {
-            if (pos1 == pos2 && pos2 == pos3)
-            {
+        if (pos1 != "" && pos2 != "" && pos3 != "") {
+            if (pos1 == pos2 && pos2 == pos3) {
                 showWinner(pos1);
                 return true;
             }
